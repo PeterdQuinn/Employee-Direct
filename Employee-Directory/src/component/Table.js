ac377fb237e6f8d942881736a7bbd4ac07f91949
Your Name <peterdquinnsr@gmail/com>
2022-06-19 13:50:15 -0700

    mobile

--- Employee-Directory/src/component/Table.js
+++ Employee-Directory/src/component/Table.js
@@ -2,32 +2,48 @@ import React from "react";
 import TableRow from "./TableRow";
 
 function Table(props) {
-    const sortTable = (event)=>{
+    const sortTable = (event) => {
         const header = event.target.textContent
         props.sort(header);
-    } 
-    return ( 
-        
-        <table className="table table-s table-Light justify-content-center">
+    }
+    return (
+
+
+        <table className="table table table-Light justify-content-left">
+
             <thead>
                 <tr>
-                    <th scope="col"></th>
-                    <th scope="col" onClick={sortTable}>Name</th>
-                    <th scope="col" onClick={sortTable}>Email</th>
-                    <th scope="col" onClick={sortTable}>Age</th>
-                    <th scope="col" onClick={sortTable}>State</th>
-                    <th scope="col" onClick={sortTable}>Phone</th>
-                </tr>
-            </thead>
-            <tbody>
-                {/* {this.state.friends.map((friend) => <FriendCard key={friend.id} removeFriend={this.removeFriend} {...friend} />)} */}
-                {props.employees.map(((employee) => {
-                    return <TableRow key={employee.login.uuid} {...employee} />
-                }))}
-            </tbody>
-        </table>
-        
-    )
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-xl">
+
+
+                                <th scope="col-"></th>
+                                <th scope="col" onClick={sortTable}>Name</th>
+                                <th scope="col" onClick={sortTable}>Email</th>
+                                <th scope="col" onClick={sortTable}>Age</th>
+                                <th scope="col" onClick={sortTable}>State</th>
+                                <th scope="col" onClick={sortTable}>Phone</th>
+
+
+                              </div>
+                              </div>
+                              </div>
+                                
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            {/* {this.state.friends.map((friend) => <FriendCard key={friend.id} removeFriend={this.removeFriend} {...friend} />)} */}
+                            {props.employees.map(((employee) => {
+                                return <TableRow key={employee.login.uuid} {...employee} />
+                            }))}
+                        </tbody>
+                    </table>
+
+
+                    )
 }
 
-export default Table;
+                    export default Table;
--- Employee-Directory/src/component/TableRow.js
+++ Employee-Directory/src/component/TableRow.js
@@ -3,16 +3,24 @@ import React from "react";
 function TableRow(props) {
     return (
         <tr>
+               
+               <div class="container-fluid  p-3 my-3 bg-dark text-white">
+               <div class="row">
+                    
             <td>
-            <img src={props.picture.thumbnail} alt="image of employee" className="img-thumbnail">
+            <img src={props.picture.thumbnail} alt="image of employee"  className="img-thumbnail">
             
             </img>
             </td>
+            
             <td>{props.name.first + " " + props.name.last}</td>
             <td>{props.email}</td>
             <td>{props.registered.age}</td>
             <td>{props.location.state}</td>
             <td>{props.phone}</td>
+            </div>
+            </div>
+            
         </tr>
     )
 }
